7708452aefec06122091ca81e9e74af62bbfad01
Sanimesh96 <sanimesh96@gmail.com>
2023-11-02 16:43:15 +0530

    Link added to letter Page

--- src/CoverPage.jsx
+++ src/CoverPage.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function CoverPage() {
+  const navigate = useNavigate();
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShowButton(true), 62000);
+  }, []);
+
+  function handleRedirect() {
+    navigate("/letter");
+  }
 
   return (
-        <iframe
-          src="https://sanimesh96.github.io/"
-          width="100%"
-          title="project-x-cover"
-        ></iframe>
+    <>
+      <iframe
+        src="http://sanimesh96.github.io"
+        width="100%"
+        title="project-x-cover"
+        id="project-x-cover"
+      ></iframe>
+      <div className="redirect-btn" hidden={!showButton}>
+        <p onClick={handleRedirect}>
+          Or Click Me to go Forward{" "}
+        </p>
+      </div>
+    </>
   );
 }
